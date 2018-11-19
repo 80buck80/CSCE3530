59afb55655e1e5995fcbfd5cf6c1bee0fa65d661
adb0026 <adb0026@sys-its-g9sfs22.unt.ad.unt.edu>
2018-11-19 15:07:57 -0600

    Testing server string split

--- homework3/server.c
+++ homework3/server.c
@@ -28,6 +28,9 @@ int main(int argc, char *argv[])
     //Server IP and ID variables
     char *ip;
     int id;
+    // Token variable used when splitting strings
+    char *token;
+    char *clientMessage[3][32];
     //Server Response String Template
     char *dhcpRespose = "yiaddr: %s\nTransaction ID: %d\nLifetime: 3600 secs";
 
@@ -70,7 +73,20 @@ int main(int argc, char *argv[])
 
         //print details of the client/peer and the data received
         //printf("Received packet from %s, port number:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Client has sent: \n%s\n", buf);
+        token = strtok(buff, "\n");
+        i = 0;
+        while (token != NULL)
+        {
+          printf("%s\n", token);
+          clientMessage[i] = token;
+          token = strtok(NULL, "\n");
+        }
+
+        for(i = 0; i < 3; i ++)
+        {
+          printf("\nClient Message:\n");
+          printf("%s\n", clientMessage[i]);
+        }
 
 
         ip = getIP(&ipCount);
